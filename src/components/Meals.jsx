b7e603d9fd13b909a5a048d7d95b094e11a23575
seodoha <doha.seo89@gmail.com>
2024-10-10 16:04:37 +0900

    last

---
+++ src/components/Meals.jsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from 'react';
+import MealItem from './MealItem';
+import useHttp from '../hooks/useHttp';
+import Error from './Error';
+
+const requestConfig = {};
+
+export default function Meals() {
+  const {
+    data: loadedMeals,
+    isLoading,
+    error,
+  } = useHttp('http://localhost:3000/meals', requestConfig, []);
+  // const [loadedMeals, setLoadedMeals] = useState([]);
+  // useEffect(() => {
+  //   async function fetchMeals() {
+  //     const response = await fetch('http://localhost:3000/meals');
+  //     if (!response.ok) {
+  //       // ...
+  //     }
+
+  //     const meals = await response.json();
+  //     setLoadedMeals(meals);
+  //   }
+  //   fetchMeals();
+  // }, []);
+
+  if (isLoading) {
+    return <p className='center'>Fetching meals...</p>;
+  }
+
+  if (error) {
+    return <Error title='Failed to fetch meals' message={error} />;
+  }
+
+  return (
+    <ul id='meals'>
+      {loadedMeals.map((meal) => (
+        <MealItem key={meal.id} meal={meal} />
+      ))}
+    </ul>
+  );
+}
